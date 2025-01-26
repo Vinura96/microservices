d859d91d7724d8497d408b025102dd0a686e689b
Vinura <vinuraran.16@cse.mrt.ac.lk>
2025-01-26 20:09:12 +0530

    configure the discovery service

--- discovery/src/main/java/com/microservices/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/microservices/discovery/DiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.microservices.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer //enable auto configuration of the eureka server
 @SpringBootApplication
 public class DiscoveryApplication {
 
